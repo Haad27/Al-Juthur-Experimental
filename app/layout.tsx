f601f35d46d36f3c52f47faf9b9e27b36ae02888
Haad majeed <haadmajeedch27@gmail.com>
2026-09-05 18:26:09 +0500

    feat: universal update of all web, app, PWA, favicon, and OpenGraph icons to Concept 1

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,7 @@ export const metadata = {
   publisher: "Al-Juthur",
   icons: {
     icon: [
+      { url: "/assets/favicon/favicon.svg", type: "image/svg+xml" },
       { url: "/assets/favicon/favicon-32x32.png", sizes: "32x32", type: "image/png" },
       { url: "/assets/favicon/favicon-16x16.png", sizes: "16x16", type: "image/png" },
     ],
